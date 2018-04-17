bbe50653011a8e77e71e317e9fb9d1aa496693ca
iivanovw7 <9iiiuk2@gmail.com>
2018-04-18 00:45:57 +0300

    tested one report

--- js/data.js
+++ js/data.js
@@ -2,14 +2,11 @@ var xml = "<datetime><timestamp>20180411030425</timestamp><day>20180408</day><da
   xmlDoc = $.parseXML( xml ),
   $xml = $( xmlDoc ),
   $day = $xml.find( "day" );
- 
-// Append "RSS Title" to #someElement
+
 $( "#someElement" ).append( $day.text() );
- 
-// Change the title to "XML Title"
+
 $day.text( "XML Title" );
- 
-// Append "XML Title" to #anotherElement
+
 $( "#anotherElement" ).append( $day.text() );
 
 
--- js/infoParser.js
+++ js/infoParser.js
@@ -1,10 +1,14 @@
+//getting page onload states
 $( document ).ready(function() {
+    $(".measuringBlock").hide();
     $("#FullChartBtn").hide();
     $("#placeHolder").append("Для вывода отчета загрузите файл с расширением XML");
-    
 });
 
 
+
+//upload xml file function
+//calling parsing function with measuring point id
 
 var openFile = function(event) {
 	var input = event.target;
@@ -13,7 +17,6 @@ var openFile = function(event) {
 	var onload = function(event) {
         text = reader.result;
         parseInfo(text);
-        parseReport(text);
         reportParser(text, 0);
         reportParser(text, 1);
         reportParser(text, 2);
@@ -23,12 +26,12 @@ var openFile = function(event) {
 	reader.onload = onload;
 	reader.readAsText(input.files[0]);
     $("#placeHolder").remove();
-    $("#FullChartBtn").show();
+    $("#FullChartBtn").fadeIn();
+    $(".measuringBlock").fadeIn();
 };
 
 
-
-
+//show report information block
 var parseInfo = function(text) {
 	var xmlDoc = $.parseXML(text),
 		$xml = $(xmlDoc),
@@ -61,18 +64,31 @@ var parseInfo = function(text) {
     
 };
 
-var showTitles = function() {
 
-    $("#chartAP60").append("Активная энергия (кВт*ч) / Время окончания периода (часы)");
-    $("#chartAP120").append("Активная энергия (кВт*ч) / Время окончания периода (часы)");
-    $("#chartRP60").append("Реактивная энергия (кВар*ч) / Время окончания периода (часы)");
-    $("#chartRP120").append("Реактивная энергия (кВар*ч) / Время окончания периода (часы)");
+var ChartAP60_1 = [];
+var ChartRP60_1 = [];
 
-};
+var ChartAP60_2 = [];
+var ChartRP60_2 = [];
 
+var ChartAP60_3 = [];
+var ChartRP60_3 = [];
+
+var ChartAP60_4 = [];
+var ChartRP60_4 = [];
+
+
+
+//xml parsing function
 var reportParser = function(text, pointNumber) {
     var xmlDoc = $.parseXML(text);
     var reportIndex = 0;
+    var chartA = "";
+    var chartB = "";
+    var chartC = "";
+    var chartD = "";
+    var wideChart = "";
+
 
     var measuringPoint = xmlDoc.getElementsByTagName("measuringpoint")[pointNumber];
     var measuringChannel01  = measuringPoint.childNodes[0];
@@ -80,26 +96,45 @@ var reportParser = function(text, pointNumber) {
 
     $getNumber = $(xmlDoc.getElementsByTagName("measuringpoint")[pointNumber]);
     var number = $getNumber.attr('code');
-    console.log(number)
 
     if (number == 782130212218101) {
         $("#inputNumber"+pointNumber).append("РУ-1  0,4кВ");
         reportIndex = 1;
+        chartA = "#chartAP600";
+        chartB = "#chartAP1200";
+        chartC = "#chartRP600";
+        chartD = "#chartRP1200";
+        wideChart = "#wideChart0";
     } else  
 
     if (number == 782130212218401) {
         $("#inputNumber"+pointNumber).append("РУ-4  0,4кВ");
         reportIndex = 4;
+        chartA = "#chartAP601";
+        chartB = "#chartAP1201";
+        chartC = "#chartRP601";
+        chartD = "#chartRP1201";
+        wideChart = "#wideChart1";
     } else
 
     if (number == 782130212218301) {
         $("#inputNumber"+pointNumber).append("РУ-3  0,4кВ");
         reportIndex = 3;
+        chartA = "#chartAP602";
+        chartB = "#chartAP1202";
+        chartC = "#chartRP602";
+        chartD = "#chartRP1202";
+        wideChart = "#wideChart2";
     } else
 
     if (number == 782130212218201) {
         $("#inputNumber"+pointNumber).append("РУ-2  0,4кВ");
         reportIndex = 2;
+        chartA = "#chartAP603";
+        chartB = "#chartAP1203";
+        chartC = "#chartRP603";
+        chartD = "#chartRP1203";
+        wideChart = "#wideChart3";
     }
 
     var fullWidthLabels = [];
@@ -144,75 +179,7 @@ var reportParser = function(text, pointNumber) {
     $("#sumtableAPRP"+pointNumber).append("<tr>"+"<th>"+"Активная энергия"+"</th>"+"<th>"+"Реактивная энергия"+"</th>"+"</tr>"+
     "<tr>"+"<th>"+chartAP_sum+" кВт*ч"+"</th>"+"<th>"+chartRP_sum+" кВар*ч"+"</th>"+"</tr>");
 
-
-}
-
-
-var parseReport = function(text) {
-
-    var xmlDoc = $.parseXML(text);
-    var reportIndex = 0;
-
-    var measuringPoint = xmlDoc.getElementsByTagName("measuringpoint")[0];
-    var measuringChannel01  = measuringPoint.childNodes[0];
-    var measuringChannel03  = measuringPoint.childNodes[1];
-
-
-    $getNumber = $(xmlDoc.getElementsByTagName("measuringpoint")[0]);
-    var number = $getNumber.attr('code');
-
-
-    if (number = 782130212218101) {
-        $("#inputNumber").append("РУ-1  0,4кВ");
-        reportIndex = 1;
-    }
-
-
-
-    var fullWidthLabels = [];
-    var chartAP = [];
-    var chartRP = [];
-
-    chartAP.push(null);
-    chartRP.push(null);
-
-    var chartAP_sum = 0;
-    var chartRP_sum = 0;
-
-
-    for (var i = 0; i < measuringChannel01.childNodes.length; i++) {
-
-
-        $y = $(measuringChannel01.childNodes[i]);
-        $z = $(measuringChannel03.childNodes[i]);
-        $value01 = $y.find("value");
-        $value03 = $z.find("value");
-        var start = $y.attr('start');
-        var end = $y.attr('end');
-        var StartHr = start.substring(0,2);
-        var StartMin = start.substring(2,4);
-        var EndHr = end.substring(0,2);
-        var EndMin = end.substring(2,4);
-        chartAP.push($value01.text());
-        chartRP.push($value03.text());
-        fullWidthLabels.push(start.substring(0,4));
-
-
-        chartAP_sum += Number($value01.text());
-        chartRP_sum += Number($value03.text());
-
-
-        $("#values").append("<tr>"+"<th>"+i+"</th>"+"<th>"+StartHr+":"+StartMin+"</th>"+"<th>"+EndHr+":"+EndMin+"</th>"+
-            "<th>"+$value01.text()+"</th>"+"<th>"+$value03.text()+"</th>"+"</tr>");
-    }
-
-
-    $("#valuesHeader").append("<tr>"+"<th>№</th>"+"<th>"+"<p>Начало периода</p>"+"</th>"+"<th>"+
-        "<p>Окончание периода</p>"+"</th>"+"<th>"+"<p>Активная энергия (кВт*ч)</p>"+"</th>"+
-        "</p>"+"<th>"+"<p>Реактивная энергия (кВар*ч)</p>"+"</th>"+"</tr>");
-
-    $("#sumtableAPRP").append("<tr>"+"<th>"+"Активная энергия"+"</th>"+"<th>"+"Реактивная энергия"+"</th>"+"</tr>"+
-        "<tr>"+"<th>"+chartAP_sum+" кВт*ч"+"</th>"+"<th>"+chartRP_sum+" кВар*ч"+"</th>"+"</tr>");
+    //variables for charts
 
     var chartAP_60 = [];
     for (var l = 0; l < chartAP.length; l = l+2) {
@@ -230,150 +197,148 @@ var parseReport = function(text) {
         chartRP_60.push(chartRP[p]);
     };
 
-
     var chartRP_120 = [];
     for (var u = 0; u < chartRP.length; u = u+4) {
         chartRP_120.push(chartRP[u]);
     };
 
+    //append chart in right bar
 
-    new Chartist.Line('#chart1', {
-        labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
-        series: [chartAP_60]
-    },
-        {
-            low: 0,
-            showArea: true,
-            axisX: {
-                fillHoles: false,
-                onlyInteger: true,
-                offset: 40
-            },
-            axisY: {
-                onlyInteger: true,
-                offset: 20
-            }
-        }
-    );
-
-
-
-    new Chartist.Line('#chart2', {
-            labels: [0,2,4,6,8,10,12,14,16,18,20,22,24],
-            series: [chartAP_120]
-        },
-        {
-            low: 0,
-            showArea: true,
-            axisX: {
-                onlyInteger: true,
-                offset: 40
-            },
-            axisY: {
-                onlyInteger: true,
-                offset: 20
-            }
-        }
-
-
-    );
-
-    new Chartist.Line('#chart3', {
-            labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
-            series: [chartRP_60]
-        },
-        {
-            low: 0,
-            showArea: true,
-            axisX: {
-                fillHoles: false,
-                onlyInteger: true,
-                offset: 40
-            },
-            axisY: {
-                onlyInteger: true,
-                offset: 20
-            }
-        }
-    );
-
-
-
-    new Chartist.Line('#chart4', {
-            labels: [0,2,4,6,8,10,12,14,16,18,20,22,24],
-            series: [chartRP_120]
-        },
-        {
-            low: 0,
-            showArea: true,
-            axisX: {
-                onlyInteger: true,
-                offset: 40
-            },
-            axisY: {
-                onlyInteger: true,
-                offset: 20
-            }
-        }
-
-
-    );
-
-
-
-    showTitles();
-
-
-    var ctx = document.getElementById("fullWidthChart").getContext('2d');
-    var fullChart01RP = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
-            datasets: [{
-                label: 'Реактивная энергия (кВар*ч)',
-                data: chartRP_60,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+        new Chartist.Line([chartA], {
+                labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
+                series: [chartAP_60]
             },
             {
-                label: 'Активная энергия (кВт*ч)',
-                data: chartAP_60,
-                borderWidth: 1
+                low: 0,
+                showArea: true,
+                axisX: {
+                    fillHoles: false,
+                    onlyInteger: true,
+                    offset: 40
+                },
+                axisY: {
+                    onlyInteger: true,
+                    offset: 20
+                }
             }
-        ]
-        },
-        
-        
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Измеряемая величина',
-                        fontSize: 20 
-                     }
-                }]
+        );
+
+        new Chartist.Line([chartB], {
+                labels: [0,2,4,6,8,10,12,14,16,18,20,22,24],
+                series: [chartAP_120]
+            },
+            {
+                low: 0,
+                showArea: true,
+                axisX: {
+                    onlyInteger: true,
+                    offset: 40
+                },
+                axisY: {
+                    onlyInteger: true,
+                    offset: 20
+                }
             }
-        }
-    });
+
+
+        );
+
+        new Chartist.Line([chartC], {
+                labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
+                series: [chartRP_60]
+            },
+            {
+                low: 0,
+                showArea: true,
+                axisX: {
+                    fillHoles: false,
+                    onlyInteger: true,
+                    offset: 40
+                },
+                axisY: {
+                    onlyInteger: true,
+                    offset: 20
+                }
+            }
+        );
+
+
+
+        new Chartist.Line([chartD], {
+                labels: [0,2,4,6,8,10,12,14,16,18,20,22,24],
+                series: [chartRP_120]
+            },
+            {
+                low: 0,
+                showArea: true,
+                axisX: {
+                    onlyInteger: true,
+                    offset: 40
+                },
+                axisY: {
+                    onlyInteger: true,
+                    offset: 20
+                }
+            }
+
+
+        );
+
+
+        var ctx = $(wideChart);
+
+        var fullChart01RP = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
+                datasets: [{
+                    label: 'Реактивная энергия (кВар*ч)',
+                    data: chartRP_60,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                },
+                    {
+                        label: 'Активная энергия (кВт*ч)',
+                        data: chartAP_60,
+                        borderWidth: 1
+                    }
+                ]
+            },
+
+
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Измеряемая величина',
+                            fontSize: 20
+                        }
+                    }]
+                }
+            }
+        });
+
+
+
 
 
 
@@ -381,5 +346,65 @@ var parseReport = function(text) {
 
 };
 
+
+/*
+
+var ctx = $(wideChart);
+
+        var fullChart01RP = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
+                datasets: [{
+                    label: 'Реактивная энергия (кВар*ч)',
+                    data: chartRP_60,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                },
+                    {
+                        label: 'Активная энергия (кВт*ч)',
+                        data: chartAP_60,
+                        borderWidth: 1
+                    }
+                ]
+            },
+
+
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Измеряемая величина',
+                            fontSize: 20
+                        }
+                    }]
+                }
+            }
+        });
+
+
+
+
+
+ */
 
 
